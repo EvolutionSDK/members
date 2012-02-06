822ead40576c26aca30587cfe5bd08c7eefa74c0
KellyLSB <kellylsbkr@gmail.com>
2012-02-06 10:24:55 -0700

    initial comit

---
+++ _bundle.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Bundles\Members;
+use Bundles\SQL\SQLBundle;
+use Exception;
+use e;
+
+class Bundle extends SQLBundle {
+	
+	public function __initBundle() {
+		e::$events->lhtml_add_hook(':members', $this);
+	}
+	
+	public function currentMember() {
+		try { return e::$session->getMembersMember(); }
+		catch(\Bundles\SQL\NoMatchException $e) { return false; }
+	}
+	
+	public function login($email, $password) {
+		$return = e::$sql->query("SELECT * FROM `members.account` WHERE `email` = '$email' AND `password` = md5('$password');")->row();
+		if($return) return $this->getMember($return)->linkSessionSession(e::$session->_id);
+		else return array('type' => 'error', 'message' => 'Email or Password was incorrect.');
+	}
+
+	public function register($email, $password) {
+		if($this->getByEmail($email))
+			return array('type' => 'error', 'messsage' => 'This email is already in use.');
+		
+		try {
+			$member = e::$members->newMember();
+			$member->email = $email;
+			$member->password = md5($password);
+			$member->save();
+			$member->linkWebapp(e::$webapp->subdomainAccount());
+		}
+		catch(Exception $e) {
+			return array('error', $e->getMessage());
+		}
+	}
+	
+	public function getByEmail($email) {
+		$return = e::$sql->query("SELECT * FROM `members.account` WHERE `email` = '$email';")->row();
+		if($return) return $this->getMember($return);
+		else return false;
+	}
+	
+	public function logout() {
+		if($tmp = $this->currentMember())
+			$tmp->unlinkSessionSession(e::$session->_id);
+
+		return true;
+	}
+
+	public function onNotAdminRedirect($to) {
+		$member = $this->currentMember();
+		if(!$member || $member->permission < 3)
+			e\redirect($to);
+	}
+	
+}
